d8a9a68dac2dceba043164e8c94593b917d1856b
SerheyBernatovich <sec1982@ukr.net>
2022-11-15 21:37:26 +0200

    add

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -5,18 +5,21 @@ import { useState, useEffect } from 'react';
 function App() {
   const [fromAmount, setFromAmount] = useState(1);
   const [toAmount, setToAmount] = useState(1);
-  const [fromCurrency, setFromCurrency] = useState('USD');
-  const [toCurrency, setToCurrency] = useState('EUR');
+  const [fromCurrency, setFromCurrency] = useState('UAH');
+  const [toCurrency, setToCurrency] = useState('USD');
   const [rates, setRates] = useState([]);
 
   useEffect(() => {
     fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
       .then((rec) => rec.json())
       .then((json) => {
-        rates.current = json.reduce((acc, val) => {
-          acc[val.cc] = val.rate;
-          return acc;
-        }, {});
+        rates.current = json.reduce(
+          (acc, val) => {
+            acc[val.cc] = val.rate;
+            return acc;
+          },
+          { UAH: 1 }
+        );
         setRates(rates.current);
       });
   }, []);
@@ -30,22 +33,22 @@ function App() {
   const fix = (number) => number.toFixed(2);
 
   const handleAmountFromChang = (fromAmount) => {
-    setToAmount(fix((fromAmount * rates[toCurrency]) / rates[fromCurrency]));
+    setToAmount(fix((fromAmount * rates[fromCurrency]) / rates[toCurrency]));
     setFromAmount(fromAmount);
   };
 
   const handleCurrencyFromChang = (fromCurrency) => {
-    setToAmount(fix((fromAmount * rates[toCurrency]) / rates[fromCurrency]));
+    setToAmount(fix((fromAmount * rates[fromCurrency]) / rates[toCurrency]));
     setFromCurrency(fromCurrency);
   };
 
   const handleAmountToChang = (toAmount) => {
-    setFromAmount(fix((toAmount * rates[fromCurrency]) / rates[toCurrency]));
+    setFromAmount(fix((toAmount * rates[toCurrency]) / rates[fromCurrency]));
     setToAmount(toAmount);
   };
 
   const handleCurrencyToChang = (toCurrency) => {
-    setFromAmount(fix((toAmount * rates[fromCurrency]) / rates[toCurrency]));
+    setFromAmount(fix((toAmount * rates[toCurrency]) / rates[fromCurrency]));
     setToCurrency(toCurrency);
   };
   return (
